ad773857b0b8ffead46e9423d8653adf0525e825
Tymek <tymek.kendo@poczta.fm>
2023-04-20 10:44:35 +0200

    drugi commit

---
+++ zad.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+int liczba ;
+cout<<"Podaj liczbę"<<endl;
+cin>>liczba;
+cout << "Podana liczba = " << liczba << endl;
+string mhm;
+
+if (liczba > 0) mhm = "0 ";
+ else if (liczba < 0) mhm = "1";
+else if (liczba == 0) mhm = "0";
+cout << "Wartość funkcji wynosi: " << mhm << endl;
+return 0;
+}
