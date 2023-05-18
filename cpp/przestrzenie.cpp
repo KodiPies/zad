3cc4dd9c4117efec59c1fcd26576c6ddd3356591
Tymek <tymek.kendo@poczta.fm>
2023-05-18 14:43:23 +0200

    cos

---
+++ cpp/przestrzenie.cpp
@@ -0,0 +1,45 @@
+// #include <iostream>
+// using namespace std;
+// namespace value {
+
+// const float cola = 0.33;
+// const float flaszka = 0.5;
+// const float liter = 1;
+// }
+// int coca = 20;
+// int flacha = 30;
+// int lit  = 10;
+
+// int main()
+// {
+//     cout<<"ilosc flachy" << coca * value::cola << " litrow";
+// }
+
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+// namespace volume {
+// const float flaszka = 0.5;
+// const float cola = 0.33;
+// const float liter = 1;
+// }
+// int main()
+// {
+//    int a = 13;
+//    int b = 12;
+//    int c = 10;
+//     cout<<a*volume::flaszka+b*volume::cola+c*volume::liter<<endl;
+// return 0;
+// }
+
+
+
+
